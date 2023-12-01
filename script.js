c9c491b0d757c75da69c56383df8c0c8c593a093
Vinicius Becaleti <vinicius.becaleti@gruponsc.com.br>
2023-12-01 11:49:31 -0300

    fix: trata erro no fetch

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 async function getVideos() {
-  const response = await fetch('http://localhost:3000/videos')
-  const videos = await response.json()
-  return videos
+  try {
+    const response = await fetch('http://localhost:3000/videos')
+    const videos = await response.json()
+    return {
+      success: true,
+      videos
+    }
+  } catch (error) {
+    return {
+      success: false,
+      error: "Couldn't get videos"
+    }
+  }
 }
 
 const videos = await getVideos()
-
 const containerVideos = document.querySelector('.videos__container')
 
-for (const video of videos) {
-  containerVideos.innerHTML += `
-    <li class="videos__item">
-      <iframe src="${video.url}"></iframe>
-      <div class="descricao-video">
-        <img class="img-canal" src="${video.imagem}" />
-        <h5 class="titulo-video">${video.titulo}</h5>
-        <p class="titulo-canal">${video.descricao}</p>
-      </div>
-    </li>
-  `
+if (!videos.success) {
+  containerVideos.innerHTML = videos.error
+}
+
+if (videos.success) {
+  for (const video of videos.videos) {
+    containerVideos.innerHTML += `
+      <li class="videos__item">
+        <iframe src="${video.url}"></iframe>
+        <div class="descricao-video">
+          <img class="img-canal" src="${video.imagem}" />
+          <h5 class="titulo-video">${video.titulo}</h5>
+          <p class="titulo-canal">${video.descricao}</p>
+        </div>
+      </li>
+    `
+  }
 }
