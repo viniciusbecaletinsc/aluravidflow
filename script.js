9611063c3503b39473347fb42f8da21be9b96d09
Vinicius Becaleti <vinicius.becaleti@gruponsc.com.br>
2023-12-04 09:26:22 -0300

    feat: adiciona filtro de video pela barra de pesquisa

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-async function getVideos() {
+async function getVideos(query) {
   try {
-    const response = await fetch('http://localhost:3000/videos')
+    const response = await fetch(`http://localhost:3000/videos?q=${query}`)
     const videos = await response.json()
     return {
       success: true,
@@ -14,24 +14,45 @@ async function getVideos() {
   }
 }
 
-const videos = await getVideos()
-const containerVideos = document.querySelector('.videos__container')
+async function showVideos(query = "") {
+  const videos = await getVideos(query)
+  const containerVideos = document.querySelector('.videos__container')
 
-if (!videos.success) {
-  containerVideos.innerHTML = videos.error
-}
+  if (!videos.success) {
+    containerVideos.innerHTML = videos.error
+  }
 
-if (videos.success) {
-  for (const video of videos.videos) {
-    containerVideos.innerHTML += `
-      <li class="videos__item">
-        <iframe src="${video.url}"></iframe>
-        <div class="descricao-video">
-          <img class="img-canal" src="${video.imagem}" />
-          <h5 class="titulo-video">${video.titulo}</h5>
-          <p class="titulo-canal">${video.descricao}</p>
-        </div>
-      </li>
-    `
+  if (videos.success) {
+    containerVideos.innerHTML = ""
+    
+    for (const video of videos.videos) {
+      containerVideos.innerHTML += `
+        <li class="videos__item">
+          <iframe src="${video.url}"></iframe>
+          <div class="descricao-video">
+            <img class="img-canal" src="${video.imagem}" />
+            <h5 class="titulo-video">${video.titulo}</h5>
+            <p class="titulo-canal">${video.descricao}</p>
+          </div>
+        </li>
+      `
+    }
   }
 }
+
+showVideos()
+
+const barraPesquisa = document.querySelector(".pesquisar__input")
+
+let delay = null
+
+function handleFiltrarVideos() {
+  clearTimeout(delay)
+  
+  delay = setTimeout(() => {
+    const query = barraPesquisa.value
+    showVideos(query)
+  }, 500)
+}
+
+barraPesquisa.addEventListener("input", handleFiltrarVideos)
